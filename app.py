5172ff84f74db31fa7935721691fcab085381d7d
toolkithub <saiaamir676@gmail.com>
2026-06-26 18:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,16 @@
-from flask import Flask, request, send_file
 import yt_dlp
-import os
 
-app = Flask(__name__)
-
-DOWNLOAD_FOLDER = "downloads"
-
-if not os.path.exists(DOWNLOAD_FOLDER):
-    os.makedirs(DOWNLOAD_FOLDER)
-
-@app.route("/")
-def home():
-    return "Toolkit YouTube Video Downloader Running"
-
-@app.route("/download")
-def download():
-
-    url = request.args.get("url")
-
-    if not url:
-        return {"error":"No URL provided"}
-
+def download_video(url):
     ydl_opts = {
-        'format': 'best',
-        'outtmpl': DOWNLOAD_FOLDER + '/video.%(ext)s'
+        "format": "bestvideo+bestaudio/best",
+        "merge_output_format": "mp4",
+        "outtmpl": "%(title)s.%(ext)s",
+        "noplaylist": True,
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        info = ydl.extract_info(url, download=True)
-        filename = ydl.prepare_filename(info)
-
-    return send_file(filename, as_attachment=True)
+        ydl.download([url])
 
 if __name__ == "__main__":
-    app.run()
+    video_url = input("Enter YouTube URL: ")
+    download_video(video_url)
